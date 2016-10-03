0eb315d9168b69ab6e4a1d6642dddaa7bc806428
Jason Butwell <jasonbutwell@gmail.com>
2016-10-03 19:59:23 +0100

    Countdown timer now working so the user is against the clock to guess the celebrities. The game ends when the timer reaches zero or the player guesses all the celebrities, whether they get them right or wrong, the game will still end. Also added some more comments and tidied up the source some more.

--- app/src/main/java/com/jasonbutwell/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/guessthecelebrity/MainActivity.java
@@ -2,6 +2,7 @@ package com.jasonbutwell.guessthecelebrity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -29,59 +30,114 @@ public class MainActivity extends AppCompatActivity {
     private String resultHTML = null;
     private DownloadHTMLTask task;
     private ImageDownloaderTask imageTask;
+    private CountDownTimer timer;
 
+    // For the pattern matching
     private Pattern p;
     private Matcher m;
 
     private List <String>celebImages;
     private List <String>celebNames;
     private List <String>answers;
+    private List <String>celebsSoFar;
 
-    int correctAnswerLocation = 0;
-    int celebrityIndex = 0;
+    // location of correct answer and the index of the celeb we need to pull (namewise and imagewise)
+    private int correctAnswerLocation = 0;
+    private int celebrityIndex = 0;
 
-    int correctAnswers = 0;
-    int answerTotal = 0;
+    // answers user has got correct and the total answers that have been answered
+    private int correctAnswers = 0;
+    private int answerTotal = 0;
+    private int quizDurationSeconds = 60;
+    private int milliSecond = 1000;
 
-    Bitmap celebrityImage = null;
+    // storage for image bitmap that we need to fetch
+    private Bitmap celebrityImage = null;
 
-    boolean inPlay;
+    // game state
+    private boolean inPlay;
 
+    // count down timer view
+    private TextView timerTextView;
+
+    // clears all the answer buttons so they are blank prior to and after play
+    private void clearAnswerButtons() {
+        for (int i=0; i < answerButtons.length; i++) {
+            answerButtons[i].setText("");
+        }
+    }
+
+    // The quiz has ended so reset everything
+    private void endQuiz() {
+        inPlay = false;
+        timerTextView.setText("Time Left: "+quizDurationSeconds+"s");
+        showStartButton(true);
+        clearAnswerButtons();
+        timer.cancel();
+    }
+
+    // Toggle between start button and image view
     private void showStartButton( boolean showStatus ) {
         Button showButton = (Button) findViewById(R.id.playButton);
         ImageView imageView = (ImageView) findViewById(R.id.celebrityImageView);
 
         if (showStatus == false)
         {
+            // Show == false means turn off show button and enable the imageview
             showButton.setVisibility(View.INVISIBLE);
             imageView.setVisibility(View.VISIBLE);
         }
         else {
+            // show == true means turn off the imageview and show the button
             showButton.setVisibility(View.VISIBLE);
             imageView.setVisibility(View.INVISIBLE);
         }
     }
 
+    // start the quiz. enabled with the start button is tapped
     public void startQuiz( View view ) {
+
+        correctAnswers = 0;
+        answerTotal = 0;
+
         inPlay = true;
+        celebsSoFar.clear();
+
         showStartButton( false );
+        getNextCelebrity();
+
+        timer = new CountDownTimer(quizDurationSeconds*milliSecond+(milliSecond/10),milliSecond) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText("Time Left: "+String.valueOf(millisUntilFinished/milliSecond)+"s");
+            }
+
+            @Override
+            public void onFinish() {
+                endQuiz();
+            }
+        }.start();
     }
 
+    // called on answer button tap
     public void selectionMade(View view ) {
-        if ( inPlay )
+        if ( inPlay == true )
             checkAnswer(view);
     }
 
+    // sets the buttons to the answers we stored
     private void setAnswerButtons() {
         for (int i=0; i < numberOfAnswers; i++ )
             answerButtons[i].setText(answers.get(i));
     }
 
+    // updates the answer view to show how many answered and how many correct
     private void setAnswerStatus() {
         TextView answerTotalView = (TextView)findViewById(R.id.QuestionNumberTextView);
         answerTotalView.setText("Celebrities correct so far: " + correctAnswers +" / " + answerTotal);
     }
 
+    // sets up the answers for the user
     private void setAnswers() {
         answers.clear();
 
@@ -93,8 +149,9 @@ public class MainActivity extends AppCompatActivity {
         String otherAnswer = "";
 
         for (int i=0; i < numberOfAnswers; i++ ) {
-            if ( i == correctAnswerLocation )             // If the index is equal to the answer location, just add in the answer
+            if ( i == correctAnswerLocation ) {            // If the index is equal to the answer location, just add in the answer
                 answers.add(answerName);
+            }
             else {                                                          // if its not the correct answer location
                 otherAnswer = getCelebrityName(getRandomCelebrityIndex());  // get another random celebrity name
 
@@ -112,44 +169,60 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        // update the answer status and the answer buttons
         setAnswerStatus();
         setAnswerButtons();
     }
 
+    // called when we tap a button to check the tag of the button with the answer id to see if correct or incorrect
     private void checkAnswer(View view) {
         String message = "";
 
         if (view.getTag().toString().equals(Integer.toString(correctAnswerLocation+1))) {
-            message = "Well done! CORRECT!";
+            message = "Well done! CORRECT!";        // Answer correct
             correctAnswers++;
         }
         else
-            message = "I'm sorry, that's WRONG!";
+            message = "I'm sorry, that's WRONG!";   // Answer wrong
 
+        // show correct / incorrect to user
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
-        answerTotal++;
+        // check we have not answered more questions than we have
+        if ( answerTotal < maxNumberOfCelebrities ) {
+            answerTotal++;
+        }
+
         getNextCelebrity();
+
+        // check to see if all the celebs have been guessed. If so, we need to end the game!
+        if ( answerTotal == maxNumberOfCelebrities )
+            endQuiz();
     }
 
+    // Gets a random index based on the number of celebrities
     private int getRandomCelebrityIndex() {
         Random rand = new Random();
         return rand.nextInt(maxNumberOfCelebrities);
     }
 
+    // gets a random location to store the correct answer in
     private int getRandomAnswerLocation() {
         Random rand = new Random();
         return rand.nextInt(numberOfAnswers);
     }
 
+    // Passes back the name based on the index
     private String getCelebrityName( int index ) {
         return celebNames.get(index);
     }
 
+    // Passes back the URL of the celeb image based on the index
     private String getCelebrityImageURL( int index ) {
         return celebImages.get(index);
     }
 
+    // Change the celeb image view to the bitmap we obtained
     private boolean setViewImageBitmap(Bitmap bitmap) {
         boolean status = false;
 
@@ -162,13 +235,15 @@ public class MainActivity extends AppCompatActivity {
         return status;
     }
 
+    // Uses pattern matching to extract the name and url of the celebrities from the HTML and store them in 2 seperate lists
     private void extractDataFromHTML() {
         p = Pattern.compile(HTMLextractionPattern);
         m = p.matcher(resultHTML);
 
-        String [] array = new String[5];
+        String [] array = new String[5];    // for storing the broken up string
         int count = 0;
 
+        // stop when we have found all the celebrities, including a counter just to be safe
         while ( m.find() && count < maxNumberOfCelebrities ) {
             String temp = m.group(1);
             array = temp.split( HTMLsplitString );
@@ -176,15 +251,27 @@ public class MainActivity extends AppCompatActivity {
             celebImages.add(array[0]); // store the extracted celebrity image url
             celebNames.add(array[1]); // store the extracted celebrity name
 
+            // Increasing the count of celebs we have extracted so far
             count++;
         }
     }
 
+    // Called to grab the next celebrity that the user can guess
     private void getNextCelebrity() {
+
         celebrityIndex = getRandomCelebrityIndex();
+
+        // Look to ensure we have unique entries and no celebrity is repeated on each game
+        for ( int i =0; i < celebsSoFar.size(); i++ )
+            while ( String.valueOf(celebrityIndex).equals(celebsSoFar.get(i) ) )
+                celebrityIndex = getRandomCelebrityIndex();
+
+        // If the celebrities index has not been used before, add it to the list to check against next time.
+        celebsSoFar.add(String.valueOf(celebrityIndex));
 
         imageTask = new ImageDownloaderTask();
 
+        // exception handling
         try {
             celebrityImage = imageTask.execute(getCelebrityImageURL(celebrityIndex)).get();
             setViewImageBitmap(celebrityImage);
@@ -195,6 +282,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // set up the answer buttons
         setAnswers();
     }
 
@@ -203,21 +291,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up lists
         celebImages = new ArrayList<String>();
         celebNames = new ArrayList<String>();
         answers = new ArrayList<String>();
+        celebsSoFar = new ArrayList<String>();
 
+        // store the answer button references in an array for easy reference throughout
         answerButtons = new Button[numberOfAnswers];
         answerButtons[0] = (Button)findViewById(R.id.button1);
         answerButtons[1] = (Button)findViewById(R.id.button2);
         answerButtons[2] = (Button)findViewById(R.id.button3);
         answerButtons[3] = (Button)findViewById(R.id.button4);
 
+        // needed to update the view of the timer
+        timerTextView = (TextView) findViewById(R.id.TimertextView);
+
+        // quiz initialise
         inPlay = false;
         showStartButton( true );
+        clearAnswerButtons();
 
+        // Async Task to grab the HTML from the web in the background
         task = new DownloadHTMLTask();
 
+        // Exception handling
         try {
             resultHTML = task.execute(htmlURL).get();
             extractDataFromHTML();
@@ -227,15 +325,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
-        getNextCelebrity();
-
-//        String name = getCelebrityName(celebrityIndex);
-//        String url = getCelebrityImageURL(celebrityIndex);
-//        System.out.println("#" + celebrityIndex + "name: " + name + " url:" + url);
-
-
-
-        System.out.println(answers);
     }
 }
